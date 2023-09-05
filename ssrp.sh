582926dd296d437098a608ac326f7009c2c47523
msaadat03 <saadatam@code.edu.az>
2023-09-05 11:25:04 +0400

    add ssl certificate for ad

--- ssrp.sh
+++ ssrp.sh
@@ -3,7 +3,9 @@ docker build -t salehhajiyev/ssrp .
 docker run -p 8001:80 \
         -d \
         --name ssrp \
+        -v $PWD/ldap.conf:/etc/ldap/ldap.conf \
         -v $PWD/hosts:/etc/hosts \
+        -v $PWD/cs401-DC-CA.cer:/etc/ssl/certs/cs401-DC-CA.cer \
         -v $PWD/ssp.conf.php:/var/www/conf/config.inc.local.php \
         -v $PWD/config.inc.php:/var/www/conf/config.inc.php \
         salehhajiyev/ssrp
