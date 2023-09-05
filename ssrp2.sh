c965a5fb40e8b24c89a0a1f5d50b9b24e4c8293c
msaadat03 <saadatam@code.edu.az>
2023-09-05 11:30:58 +0400

    add ssrp2.sh file

---
+++ ssrp2.sh
@@ -0,0 +1,9 @@
+docker run -p 8002:80 \
+	-d \
+        --name ssrp \
+        -v $PWD/ldap.conf:/etc/ldap/ldap.conf \
+        -v $PWD/hosts:/etc/hosts \
+        -v $PWD/cs401-DC-CA.cer:/etc/ssl/certs/cs401-DC-CA.cer \
+        -v $PWD/ssp.conf.php:/var/www/conf/config.inc.local.php \
+        -v $PWD/config.inc.php:/var/www/conf/config.inc.php \
+    -it docker.io/ltbproject/self-service-password:latest
